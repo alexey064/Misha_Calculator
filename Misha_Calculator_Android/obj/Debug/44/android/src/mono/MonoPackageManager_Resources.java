fc3649c7455bc8fddcb68ae3a7ce1d63cfcc2ddf
Косарев Алексей <sonic751a@gmail.com>
2019-08-27 12:24:53 +0300

    Добавил Android версию. Должно работать на Фтвкщшв 4.4

---
+++ Misha_Calculator_Android/obj/Debug/44/android/src/mono/MonoPackageManager_Resources.java
@@ -0,0 +1,10 @@
+package mono;
+public class MonoPackageManager_Resources {
+	public static String[] Assemblies = new String[]{
+		/* We need to ensure that "Misha_Calculator_Android.dll" comes first in this list. */
+		"Xamarin.Essentials.dll",
+	};
+	public static String[] Dependencies = new String[]{
+	};
+	public static String ApiPackageName = "Mono.Android.Platform.ApiLevel_19";
+}
