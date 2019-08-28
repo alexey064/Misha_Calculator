de6c0533b4b24dc1de538397a8f44e618679d37d
Косарев Алексей <sonic751a@gmail.com>
2019-08-28 14:17:03 +0300

    Синхронизация веток №1

---
+++ Misha-Calculator/Additional/KeyBoardPressProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+
+namespace Misha_Calculator.Additional
+{
+    class KeyBoardPressProcessor
+    {
+        public static void KeyBoardPress(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                
+            }
+        }
+    }
+}
---
+++ Misha-Calculator/Additional/StackExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misha_Calculator.Additional
+{
+    public static class StackExtension
+    {
+        public static bool IsEmpty(this Stack<string> stack)
+        {
+            return stack.Count == 0;
+        }
+        public static bool IsEmpty(this Stack<double> stack)
+        {
+            return stack.Count == 0; ;
+        }
+    }
+}
